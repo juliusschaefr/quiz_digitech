97a6cb4a573bc34f3ac8695a440ee5659bbc2205
juliusschaefr <285967401+juliusschaefr@users.noreply.github.com>
2026-06-09 16:43:56 +1200

    added questions

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function welcomeMessage() {
 There are 5 questions. 
 Let's go!`);
 }
+
+const questions = [
+  {
+    question: "What is the longest river in the world?",
+    answer: "Nile",
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    answer: "1912",
+  },
+  {
+    question: "What is the fastest land animal?",
+    answer: "Cheetah",
+  },
+  {
+    question: "What is the smallest prime number?",
+    answer: "2",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answer: "Au",
+  },
+];
