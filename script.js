19bf962b52417fdbd5c7ea0a3c6e5eaa23a7786e
juliusschaefr <285967401+juliusschaefr@users.noreply.github.com>
2026-06-09 16:24:35 +1200

    added welcomeMessage()

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello World!");
+function welcomeMessage() {
+  let userName = prompt("What's your name?", "Anonymous user");
+  alert(`Welcome to the quiz, ${userName}!
+    
+There are 5 questions. 
+Let's go!`);
+}
